f634d9942b4f0f6a7d8a5bb05c7ae525cdaae9ba
ajayijoshua2016@gmail.com <ajayijoshua2016@gmail.com>
2024-03-29 23:31:43 +0100

    added new

---
+++ lazzJS.js
@@ -0,0 +1,10 @@
+// let password = prompt("Please enter your password")
+//     if (password.length >= 6) {
+//       window.location = "Tour.html"
+//     }
+//     else {
+//         window.location = "Contact.html"
+//     }
+function book(){
+  window.location = "Book.html"
+}
